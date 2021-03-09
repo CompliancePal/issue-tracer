82df2abe81259b26b7a0ef5ee782afcbc9f955e6
Vlad Știrbu <vstirbu@gmail.com>
2021-03-09 15:14:16 +0200

    simplify

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     core.debug(`The event payload: ${payload}`)
 
-    core.setOutput('time', new Date().toTimeString())
     core.setOutput('payload', payload)
   } catch (error) {
     core.setFailed(error.message)
