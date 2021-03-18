2ecabf256136207630937a348768ee30d4e9d0ed
Vlad Știrbu <vstirbu@gmail.com>
2021-03-18 18:59:15 +0200

    added switch on main script

---
+++ src/actions/issues.ts
@@ -0,0 +1,62 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {IssuesOpenedEvent} from '@octokit/webhooks-definitions/schema'
+import {Issue} from '../domain/Issue'
+import {IssuesRepo} from '../repo/Issues'
+
+export const issuesHandler = async (): Promise<undefined> => {
+  const ghToken = process.env.GITHUB_TOKEN
+
+  if (ghToken === undefined) {
+    core.setFailed(`GITHUB_TOKEN not provided`)
+    return
+  }
+
+  let issue, repo, relatedIssue
+
+  switch (github.context.payload.action) {
+    case 'edited':
+    case 'opened':
+    case 'closed':
+    case 'reopened':
+      issue = Issue.fromEventPayload(
+        github.context.payload as IssuesOpenedEvent
+      )
+      core.info(`Issue ${issue.number} parsed successfuly`)
+
+      if (issue.partOf === undefined) {
+        core.info('Issue is not partOf other issues')
+        return
+      }
+
+      repo = new IssuesRepo(ghToken)
+
+      relatedIssue = await repo.get(issue.partOf)
+
+      if (relatedIssue === undefined) {
+        core.setFailed(`Action could not find the related issue `)
+        return
+      }
+      core.info(`Related issue ${relatedIssue.number} found sucessfuly`)
+
+      core.info(
+        `Related issue ${relatedIssue.number} has ${relatedIssue.subtasks.size} subtasks`
+      )
+
+      relatedIssue.addSubtask({
+        id: issue.number.toString(),
+        title: issue.title,
+        closed: issue.closed,
+        removed: false,
+        owner: issue.owner,
+        repo: issue.repo
+      })
+
+      await repo.save(relatedIssue)
+      core.info(`Related issue ${relatedIssue.number} updated sucessfuly`)
+
+      core.setOutput('partOf', issue.partOf)
+      break
+    default:
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {issuesHandler} from './actions/issues'
 // import {IssuesOpenedEvent} from '@octokit/webhooks-definitions/schema'
 // import {Issue} from './domain/Issue'
 // import {IssuesRepo} from './repo/Issues'
@@ -13,55 +14,12 @@ async function run(): Promise<void> {
   }
 
   try {
-    core.info(github.context.eventName)
-    core.info(github.context.action)
-    // let issue, repo, relatedIssue
-
-    // switch (github.context.payload.action) {
-    //   case 'edited':
-    //   case 'opened':
-    //   case 'closed':
-    //   case 'reopened':
-    //     issue = Issue.fromEventPayload(
-    //       github.context.payload as IssuesOpenedEvent
-    //     )
-    //     core.info(`Issue ${issue.number} parsed successfuly`)
-
-    //     if (issue.partOf === undefined) {
-    //       core.info('Issue is not partOf other issues')
-    //       return
-    //     }
-
-    //     repo = new IssuesRepo(ghToken)
-
-    //     relatedIssue = await repo.get(issue.partOf)
-
-    //     if (relatedIssue === undefined) {
-    //       core.setFailed(`Action could not find the related issue `)
-    //       return
-    //     }
-    //     core.info(`Related issue ${relatedIssue.number} found sucessfuly`)
-
-    //     core.info(
-    //       `Related issue ${relatedIssue.number} has ${relatedIssue.subtasks.size} subtasks`
-    //     )
-
-    //     relatedIssue.addSubtask({
-    //       id: issue.number.toString(),
-    //       title: issue.title,
-    //       closed: issue.closed,
-    //       removed: false,
-    //       owner: issue.owner,
-    //       repo: issue.repo
-    //     })
-
-    //     await repo.save(relatedIssue)
-    //     core.info(`Related issue ${relatedIssue.number} updated sucessfuly`)
-
-    //     core.setOutput('partOf', issue.partOf)
-    //     break
-    //   default:
-    // }
+    switch (github.context.eventName) {
+      case 'issues':
+        await issuesHandler()
+        break
+      default:
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
